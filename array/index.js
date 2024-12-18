33b6c2804f35882e033547f90450fd0c5742190a
arpitmakwana2901 <arpitmakwana7376@gmail.com>
2024-12-18 17:09:10 +0530

    test

---
+++ array/index.js
@@ -0,0 +1,58 @@
+// push = addb to end
+// pop = delete from end & return
+// toString = converts array to string
+// unshift = add to start
+// shift = delete from start and return
+// map = 
+// filter = 
+// foreEach = 
+// idnexOf = 
+// findIndex =
+// includes =
+// every
+// some
+// reduce 
+
+// var array1 = [10, 20, 30, 40, 50];
+//     var array2 = [60, 70, 80, 90, 100];
+//     var array3 = [...array1, ...array2];
+//    console.log(array3);
+
+
+// let car = ["FORTUNER","BMW","AUDO","SWIFT"]
+// car.push("VERNA");
+// console.log(car);
+
+// let car = ["FORTUNER","BMW","AUDO","SWIFT"]
+// car.pop();
+// console.log(car);
+
+// let car = ["FORTUNER","BMW","AUDO","SWIFT"]
+// car.unshift("xuv");
+// console.log(car);
+
+
+// Map ------------------>>>>>>>>>>>>>>>>>>>>>>>
+
+
+// let data = ["BBA","BCA","MBA","B.COM"]
+//    const storeData =  data.map((element)=>{
+//        return element+" "+"MCA"
+//     })
+
+//     console.log(storeData);
+    
+// Filter ------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// let number = [11,12,13,14,15,16,17,18,19,20]
+//    const storeNumber = number.filter((element)=>{
+//         return element >= 15;
+//     })
+    
+//     console.log(storeNumber);
+
+
+// for Each  ------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
