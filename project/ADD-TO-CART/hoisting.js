68826d0aea9ab3af4e4d9e7d7802bfc5ee0920c0
arpitmakwana2901 <arpitmakwana7376@gmail.com>
2025-01-29 16:44:42 +0530

    Javascript

---
+++ project/ADD-TO-CART/hoisting.js
@@ -0,0 +1,15 @@
+// console.log(userName)
+//  userName = batman;
+// let userName = "Batman"
+
+// console.log(userName)
+// var userName = "Batman"
+
+// console.log(userName)
+// const userName = "Batman"
+
+// temporal dead zone --  reference error
+
+// 1. Declare veriable name
+// 2. 
+
