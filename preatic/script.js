63d7e8ecb79ccd2986ef9b07f149e7b52eec7b2a
arpitmakwana2901 <arpitmakwana7376@gmail.com>
2025-02-10 14:49:15 +0530

    Javascript

---
+++ preatic/script.js
@@ -0,0 +1,24 @@
+// let array1 = [1,2,3];
+// let array2 = [4,5,6];
+
+// let array3 = [...array1,...array2];
+// console.log(array3)
+
+// let arr = [1, 2, 3];
+
+// console.log(arr);
+
+// let arr2 = [4, 5, 6];
+
+// let arr3 = [...arr2, ...arr]; //merging
+
+// console.log(arr3);
+
+// const months = ['Jan', 'March', 'April', 'June'];
+// months.splice(1, 0,'Feb');
+// // Inserts at index 1
+// console.log(months);
+
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const myBest = fruits.slice(0,3)
+console.log(myBest)
